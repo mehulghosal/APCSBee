55c8696cef01cca2124418b7c51306f8972ef02b
mehulghosal <mehul.ghosal@gmail.com>
2018-01-02 10:04:09 -0500

    apeirjoi

--- Main.java
+++ Main.java
@@ -5,8 +5,29 @@ public class Main{
 
 	public static Node[][][] grid;
 
-	public void setup(){
-		//setup grid
+	public static void setup(){
+
+	}
+
+	public static void read(){
+		try{
+			Scanner s = new Scanner(new File("test.txt"));
+			s.nextLine();
+			String firstLn = s.nextLine();
+			int size = Integer.parseInt(firstLn.split(",")[0]);
+			Node[][][] g = new Node[size][size][size];
+
+			//set up the 
+			for(int i = 0; i<15; i++){
+
+			}
+
+		}
+		catch(Exception e){
+			System.out.println("Reading failed");
+		}
+
+		grid = g;
 	}
 
 	public static void main(String[] args){
--- Node.java
+++ Node.java
@@ -1,27 +1,34 @@
 import java.util.*;
 import java.io.*;
 
-public abstract class Node{
+public class Node{
 
 	protected int x;//x coordinate
 	protected int y;//y coordinate
 	protected int z;//z coordinate
+	protected String name; //if null, the node is empty
 
 	public Node(){
 
 	}
 
 	public Node(int x, int y, int z){
-		this.x = x; this.y = y; this.z = z;
+		this.x = x; this.y = y; this.z = z; name = "Bee";
+	}
+
+	public Node(int x, int y, int z, String n){
+		this.x = x; this.y = y; this.z = z; name = n;
 	}
 
 	public int getX(){return this.x;}
 	public int getY(){return this.y;}
 	public int getZ(){return this.z;}
+	public String getName(){return name;}
 
 	public void setX(int x){this.x = x;}
 	public void setY(int y){this.y = y;}
 	public void setZ(int z){this.z = z;}
+	public void setName(String n){this.name = name;}
 
 }
 
