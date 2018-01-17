248d20f9e9f2629bd383aaf5e3ce0d9bd1f0a8d3
mehulghosal <mehul.ghosal@gmail.com>
2018-01-17 11:06:12 -0500

    woeebfo

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main{
 
 	public static Node[][][] grid;
-	public static Bee[] bees;
+	public static ArrayList<Bee> bees = new ArrayList<Bee>();
 	public static ArrayList<Node> beehives = new ArrayList<Node>();
 
 	public static void read(){
@@ -20,7 +20,6 @@ public class Main{
 
 			
 			grid = new Node[size][size][size];
-			bees = new Bee[15];
 			//sets up board
 			for(int i = 0; i<size; i++){
 				for(int j = 0; j<size; j++){
@@ -47,7 +46,7 @@ public class Main{
 				int z = Integer.parseInt(hold[2]);
 				Bee b = new Bee(x,y,z);
 				grid[x][y][z].setBee(b);
-				bees[i] = b;
+				bees.add(b);
 			}
 			int obsCount = Integer.parseInt(s.nextLine());
 			for(int i = 0; i < obsCount; i++){
@@ -66,23 +65,117 @@ public class Main{
 		}
 	}
 
+	public static void readTest(){
+		try{
+			//System.out.println("1");
+			Scanner s = new Scanner(new File("test.txt"));
+			//System.out.println("2");
+			s.nextLine();
+			//System.out.println("3");
+			String firstLn = s.nextLine();
+			//System.out.println("4");
+			int size = Integer.parseInt(firstLn.split(",")[0]);
+
+			
+			grid = new Node[size][size][size];
+			//sets up board
+			for(int i = 0; i<size; i++){
+				for(int j = 0; j<size; j++){
+					for(int k = 0; k<size; k++){
+						grid[i][j][k] = new Node(i,j,k);
+					}
+				}
+			}
+			//sets up beehives
+			for(int i = 0; i<3; i++){
+				String[] hold = s.nextLine().split(",");
+				int x = Integer.parseInt(hold[0]);
+				int y = Integer.parseInt(hold[1]);
+				int z = Integer.parseInt(hold[2]);
+				grid[x][y][z] = new Node(x, y, z, "Beehive"); 
+				beehives.add(grid[x][y][z]);
+			}
+			//System.out.println(2);
+			//sets up the bees
+			for(int i = 0; i<3; i++){
+				//System.out.println(i);
+				String[] hold = s.nextLine().split(",");
+				int x = Integer.parseInt(hold[0]);
+				int y = Integer.parseInt(hold[1]);
+				int z = Integer.parseInt(hold[2]);
+				Bee b = new Bee(x,y,z);
+				grid[x][y][z].setBee(b);
+				bees.add(b);
+			}
+			//System.out.println(3);
+			int obsCount = Integer.parseInt(s.nextLine());
+			for(int i = 0; i < obsCount; i++){
+				String[] hold = s.nextLine().split(",");
+				int x = Integer.parseInt(hold[0]);
+				int y = Integer.parseInt(hold[1]);
+				int z = Integer.parseInt(hold[2]);
+				grid[x][y][z].setName("Occupied");
+				//System.out.println(x+" "+y+" "+z);
+			}
+		}
+		catch(Exception e){
+			System.out.println("error: " + e.getMessage());
+		}
+	}
+
 	public static void move(Bee b, int x, int y, int z){
-		grid[b.getX()][b.getY()][b.getZ()].setBee(null);//sets old Node's bee to Null
+		getNode(b).setBee(null);//sets old Node's bee to Null
 		grid[x][y][z].setBee(b);//new node's bee set to b
 		b.setX(x); b.setY(y); b.setZ(z);//b's x,y,z set
 	}
+
+	public static Node getNode(Bee b){
+		return grid[b.getX()][b.getY()][b.getZ()];
+	}
+
+	public static ArrayList<Node> around(Node center){
+		int x = center.getX(); int y = center.getY(); int z = center.getZ();
+		ArrayList<Node> around = new ArrayList<Node>();
+		for(int i = -1; i<2; i++){
+			for(int j = -1; j < 2; j++){
+				for(int k = -1; k<2; k++){
+
+					if(!(i==0 && j == 0 && k == 0)){
+						around.add(grid[x+i][y+i][z+i]);
+					}
+				}
+			}
+		}
+
+		return around;
+	}
+
 	//do floodfill
-	public static void algorithm(Bee b){
+	//returns number of moves needed for bee to move from starting location to end node
+	public static int algorithm(Bee b){
 		Node goal = beehives.get(0);
 		beehives.remove(goal);
-		//while(goal.){
+		Node start = getNode(b);
+		start.setVal(0);//signifies that first node has a value of 0 from the start
+		ArrayList<Node> visited = new ArrayList<Node>();
+		ArrayList<Node> aroundArr = new ArrayList<Node>();
+		Node current = start;
 
-	//	}
+		while(!goal.getBee().equals(b)){
+
+			aroundArr = around(current);
+			for(Node n: aroundArr){
+				n.setVal(current.getVal()+1);
+			}
+
+		}
+		return 0;
 	}
 
 	public static void main(String[] args){
 
-		read();
+		Main.readTest();
+
 
 	}
 
--- Node.java
+++ Node.java
@@ -8,51 +8,34 @@ public class Node{
 	private int z;//z coordinate
 	private String name; //if null, the node is empty
 	private Bee b;
-
-	private Node parentNode;//the node that connects to this
-	private int gCost;//distance from starting Node
-	private int hCost;//cost to end Node
-	private int fCost;//sum of g and h
-
+	private int value;//numebr of moves from start
 
 	public Node(){
 
 	}
 
 	public Node(int x, int y, int z){
-		this.x = x; this.y = y; this.z = z; name = "Bee";
+		this.x = x; this.y = y; this.z = z; value = 0;
 	}
 
 	public Node(int x, int y, int z, String n){
-		this.x = x; this.y = y; this.z = z; name = n;
+		this.x = x; this.y = y; this.z = z; name = n; value = 0;
 	}
 
-	public void calcCost(Node start, Node end){
-		gCost = (int) Math.sqrt((Math.pow(this.x - start.x, 2) + Math.pow(this.y - start.y, 2) + Math.pow(this.z - start.z, 2)));
-		hCost = (int) Math.sqrt((Math.pow(this.x - end.x, 2) + Math.pow(this.y - end.y, 2) + Math.pow(this.z - end.z, 2)));
-		fCost = gCost + hCost;
-	}
 
 	public int getX(){return this.x;}
 	public int getY(){return this.y;}
 	public int getZ(){return this.z;}
-	public String getName(){return name;}
+	public String getName(){return this.name;}
 	public Bee getBee(){return b;}
-	public Node getParent(){return parentNode;}
-	public int getfCost(){return fCost;}
-	public int getgCost(){return gCost;}
-	public int gethCost(){return hCost;}
-
+	public int getVal(){return this.value;}
+	
 	public void setX(int x){this.x = x;}
 	public void setY(int y){this.y = y;}
 	public void setZ(int z){this.z = z;}
 	public void setName(String n){this.name = name;}
 	public void setBee(Bee b){this.b = b;}
-	public void setParent(Node n){this.parentNode = n;}
-	// public void setfCost(int i){this.fCost = i;}
-	// public void setgCost(int i){this.gCost = i;}
-	// public void sethCost(int i){this.hCost = i;}
-
+	public void setVal(int i){this.value = i;}
 
 }
 
